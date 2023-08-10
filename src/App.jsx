bf0e2674dc3e59875804c58709b227dbb73b8963
Ignacio Bergantiños <bergantinos.ignacio@gmail.com>
2023-08-10 11:42:28 -0300

    feat: Add fetching and storage functions

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,40 @@
+import { useState } from "react";
 import WeatherForm from "./components/weatherForm";
+import WeatherInfo from "./components/weatherInfo";
 
 const App = () => {
-	return <WeatherForm />;
+	const [info, setInfo] = useState({});
+
+	const handleSubmit = async (e) => {
+		const value = e.target[0].value;
+		e.preventDefault();
+		const data = await fetchData(value);
+
+		await setInfo({
+			region: `Region: ${data.location.name}, ${data.location.country}`,
+			date: `Date: ${data.location.localtime}`,
+			temp: `Temp: ${data.current.temp_c} Cº`,
+			humidity: `Humidity: ${data.current.humidity} %`,
+			uv: `UV: ${data.current.uv}`,
+			windSpeed: `Wind Speed: ${data.current.wind_kph} Km/H`,
+		});
+
+		console.log(info);
+	};
+
+	const fetchData = async (zone) => {
+		const res = await fetch(
+			`http://api.weatherapi.com/v1/current.json?key=d68ca9a3462348eaab9190033230908&q=${zone}&aqi=yes`
+		);
+		return await res.json();
+	};
+
+	return (
+		<div>
+			<WeatherForm submit={handleSubmit} />
+			<WeatherInfo data={info} />
+		</div>
+	);
 };
 
 export default App;
